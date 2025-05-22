8af9b0cd9bb7960ea27d7fe031925d8e932e0221
Pedro H. Silva <pedrobhr7@gmail.com>
2025-05-22 17:18:13 -0300

    Primeiro commit

---
+++ src/main/java/org/example/bsaas/BSaaSApplication.java
@@ -0,0 +1,13 @@
+package org.example.bsaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BSaaSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bsaas/BSaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bsaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BSaaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
